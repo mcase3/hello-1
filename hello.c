b2e07b90ed52cbe475aa0559bea7ce06294ae049
aaldana8 <66578657+aaldana8@users.noreply.github.com>
2020-06-09 08:59:08 -0500

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Andrew Aldana
+ *         aaldana8@gmail.com
+ * Date: 2020/03/26
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Andrew Aldana\n");
+
+  return 0;
+}
